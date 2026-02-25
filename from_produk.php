f18087d924f127c2dde417d37cfda88d9b8e4629
acregina646-ship-it <acregina646@gmail.com>
2026-02-25 14:22:46 +0700

    Fix HTML structure in from_produk.php

--- from_produk.php
+++ from_produk.php
@@ -4,20 +4,16 @@
     <title>Form Produk</title>
 </head>
 <body>
-
 <h2>Input Data Produk</h2>
 
 <form action="proses_produk.php" method="POST">
-
 Nama Produk :
 <input type="text" name="nama"><br><br>
-
 Harga :
 <input type="number" name="harga"><br><br>
-
 <input type="submit" value="Simpan">
-
 </form>
 
 </body>
+
 </html>
